61f6561bd3c7f514415965e210552e7af3ac51ea
Pietro Piva Vieira <pietro.developer@gmail.com>
2026-05-29 02:24:07 +0000

    chore: upgrade prisma

--- src/shared/database/prisma.ts
+++ src/shared/database/prisma.ts
@@ -1,6 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@PrismaGen/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from '../config/env.js';
 
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
 export const prisma = new PrismaClient({
-	log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+	adapter,
+	log:
+		env.NODE_ENV === 'development'
+			? ['query', 'info', 'warn', 'error']
+			: ['error'],
 });
